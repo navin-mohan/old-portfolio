e7e2216baf26d5b35099da9e8241df9941392e8e
Navin Mohan <navinmohan81@gmail.com>
2017-09-03 18:03:02 +0530

    new build: new build script

--- _site/build_and_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-jekyll build
-cp ./CNAME ./_site/CNAME
-git add .
-git commit -m "new build: $1"
-git push origin src
-git subtree push --prefix _site origin master
--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 jekyll build
 cp ./CNAME ./_site/CNAME
+rm ./_site/build_and_deploy.sh
 git add .
 git commit -m "new build: $1"
 git push origin src
